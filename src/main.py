04a777d2b460acedc8189deb418a65d80476ac81
loveyousomuch <loveyousomuch554@gmail.com>
2022-04-28 02:05:16 +0300

    server static && temp plug

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 from host_info import get_host
 from os import path
 
@@ -8,12 +8,31 @@ PORT = 8080
 
 app = Flask(__name__, static_url_path='', static_folder=static_dir)
 
-# redirect to home page 
+# send home page 
 @app.route("/")
-def send_static():
+def home_page():
     try:
         return send_from_directory(static_dir, 'index.html')
     except Exception as err:
+        print("home_page", err)
+        return 'Server error', 500
+
+@app.route("/temp")
+def temp_page():
+    try:
+        return send_from_directory(static_dir, 'temp.html')
+    except Exception as err:
+        print("temp_page", err)
+        return 'Sever error', 500
+
+# get static files
+@app.route("/static/<path:path>")
+def send_static(path):
+    try:
+        p = static_dir + '/static' 
+        return send_from_directory(p, path)
+    except Exception as err:
+        print("send_static", err)
         return 'Server error', 500
 
 # handle 404 error
@@ -22,6 +41,7 @@ def page_not_found(e):
     try:
         return send_from_directory(static_dir, '404page.html')
     except Exception as err:
+        print("page_not_found", err)
         return '<h1 style="text-align: center;">Server error :(</h1>', 500
 
 if __name__ == "__main__":
